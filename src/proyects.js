4d3ac806712c3368116e93e473f0722b22de7557
DarioPC <darioquintero73@hotmail.com>
2024-06-08 03:43:36 -0300

    v1.6

--- src/proyects.js
+++ src/proyects.js
@@ -1,3 +1,7 @@
+import DevTools1 from "./img/DevTools1.jpg";
+import DevTools2 from "./img/DevTools2.jpg";
+import DevTools3 from "./img/DevTools3.jpg";
+
 import eGym1 from "./img/E-GYM1.jpg";
 import eGym2 from "./img/E-GYM2.jpg";
 import eGym3 from "./img/E-GYM3.jpg";
@@ -9,13 +13,22 @@ import spaceDogs3 from "./img/SpaceDogs3.jpg";
 
 const proyects = [
   {
+    name: "DevTools",
+    date: "Junio 2024",
+    images: [DevTools1, DevTools2, DevTools3],
+    description:
+      "Herramientas para el desarrollador, webs para practicar y hacer deploys, cursos, documentacion, y mas. Todo en un solo lugar.",
+    technologies: "JavaScript, React, Zustand, Tailwind",
+    urls: { name: "Web", path: "https://dev-tools-sable.vercel.app/" },
+  },
+  {
     name: "E-GYM",
     date: "Mayo 2024",
     images: [eGym1, eGym2, eGym3, eGym4],
     description:
       "Este proyecto incluye inicio de sesión con Google Auth junto con autenticación y encriptado de contraseñas, Slides, Barra de búsqueda, Tienda de productos, Carrito, Compra con Mercado Pago, Filtrado de productos, Comentarios de productos. Panel de administrador con vistas de: Productos, Usuarios, Cupones, Ventas y Estadísticas.",
     technologies:
-      "React, Tailwind, JavaScript, Google Auth, Cloudinary, JWT, Node.js, Express, MongoDB, Mongoose",
+      "JavaScript, React, Tailwind, Google Auth, Cloudinary, JWT, Node.js, Express, MongoDB, Mongoose",
     urls: { name: "Web", path: "https://frontend-pf-three.vercel.app/" },
   },
   {
@@ -25,7 +38,7 @@ const proyects = [
     description:
       "Desarrollé una aplicación interactiva con temática espacial dirigida a los amantes de los perros, que permite obtener, guardar y visualizar información detallada sobre diferentes razas de perros. Filtros, Formulario, Paginado, Manejo de datos con Redux, Base de datos SQL",
     technologies:
-      "React & Redux, CSS, JavaScript, Node.js, Express, PostgreSQL, Sequelize",
+      "JavaScript, CSS, React & Redux, Node.js, Express, PostgreSQL, Sequelize",
     urls: {
       name: "Repositorio",
       path: "https://github.com/Dario-Quintero/PI-Dogs",
